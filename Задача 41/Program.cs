ce09a13771f3b23d6f8369c4b8c24d7f3f9bff6a
Stas <www.stas1996@mail.ru>
2023-03-30 13:00:10 +0300

    задача 41

---
+++ Задача 41/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите числа: ");
+int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+int sum = 0;
+foreach (int element in size)
+{
+    if (element > 0)
+        sum += 1;
+}
+Console.Write(sum);
